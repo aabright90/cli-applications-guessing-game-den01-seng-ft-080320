801cacec9aeadbfbcfa415d2a979c48d3cf47e8e
Austen Bright <pureaustenbright@gmail.com>
2020-04-19 00:31:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,4 +3,8 @@ def run_guessing_game
   num = rand(1..6)
   puts "Guess a number between 1 and 6"
   input = gets.chomp
+  if input == num
+    puts "You guessed the correct number!"
+    
+  end
 end
