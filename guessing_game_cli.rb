d2b4c4f1a662165832d6cffac2ad2365f156a4f0
Austen Bright <pureaustenbright@gmail.com>
2020-04-19 00:32:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,6 +5,7 @@ def run_guessing_game
   input = gets.chomp
   if input == num
     puts "You guessed the correct number!"
-
+  elsif input != num
+    puts "Sorry! The computer guessed #{num}."
   end
 end
